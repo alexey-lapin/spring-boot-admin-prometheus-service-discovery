6a716682874a2131017132e5a578018cac44de02
Alexey Lapin <alexey-lapin@protonmail.com>
2023-02-03 21:12:45 +0100

    implement app

--- sbapsd-app/build.gradle.kts
+++ sbapsd-app/build.gradle.kts
@@ -1,10 +1,13 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "2.7.7"
+    id("io.spring.dependency-management") version "1.1.0"
 }
 
 dependencies {
     implementation(project(":sbapsd-starter"))
-    implementation("org.springframework.boot:spring-boot-starter-web:2.7.7")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
--- sbapsd-app/src/main/java/com/github/alexeylapin/sbapsd/app/App.java
+++ sbapsd-app/src/main/java/com/github/alexeylapin/sbapsd/app/App.java
@@ -1,9 +1,12 @@
-package com.github.alexeylapin.sbapsd;
+package com.github.alexeylapin.sbapsd.app;
 
+import com.github.alexeylapin.sbapsd.config.Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(Config.class)
 public class App {
 
     public static void main(String[] args) {
