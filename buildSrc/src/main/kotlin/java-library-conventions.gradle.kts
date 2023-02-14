f15c931d3023827b90c93c2d9559ed85e8d648dc
Alexey Lapin <alexey-lapin@protonmail.com>
2023-02-14 16:38:27 +0100

    implement gradle publishing

--- buildSrc/src/main/kotlin/java-library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/java-library-conventions.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     `java-library`
     `maven-publish`
+    `signing`
     id("java-base-conventions")
 }
 
@@ -10,10 +11,23 @@ publishing {
             from(components["java"])
         }
     }
-//    repositories {
-//        maven {
-//            name = "myOrgPrivateRepo"
-//            url = uri("build/my-repo")
-//        }
-//    }
+    repositories {
+        maven {
+            name = "OSSRH"
+            setUrl("https://oss.sonatype.org/service/local/staging/deploy/maven2")
+            credentials {
+                username = System.getenv("OSSRH_USER") ?: return@credentials
+                password = System.getenv("OSSRH_PASSWORD") ?: return@credentials
+            }
+        }
+    }
+}
+
+signing {
+    val key = System.getenv("SIGNING_KEY") ?: return@signing
+    val password = System.getenv("SIGNING_PASSWORD") ?: return@signing
+    val publishing: PublishingExtension by project
+
+    useInMemoryPgpKeys(key, password)
+    sign(publishing.publications)
 }
