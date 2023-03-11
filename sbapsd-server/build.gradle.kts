501aa254e58a5c75478dd2e0b7d049e7bef50484
Alexey Lapin <alexey-lapin@protonmail.com>
2023-03-11 14:13:24 +0100

    fix bom handling

--- sbapsd-server/build.gradle.kts
+++ sbapsd-server/build.gradle.kts
@@ -1,29 +1,21 @@
-import io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension
-
 plugins {
     id("sbapsd.java-library-conventions")
-    alias(libs.plugins.spring.boot.v2) apply false
-}
-
-apply(plugin = "io.spring.dependency-management")
-
-the<DependencyManagementExtension>().apply {
-    imports {
-        mavenBom(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES)
-    }
 }
 
 description = "Spring Boot Admin Prometheus Service Discovery Server"
 
 dependencies {
+    annotationProcessor(platform(libs.spring.boot.v2.bom.get()))
     annotationProcessor("org.projectlombok:lombok")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
-    compileOnly("org.projectlombok:lombok")
+    compileOnly(platform(libs.spring.boot.v2.bom.get()))
     compileOnly("de.codecentric:spring-boot-admin-server:${libs.versions.sba.v2.get()}")
+    compileOnly("org.projectlombok:lombok")
     compileOnly("org.springframework.boot:spring-boot-starter-web")
 
-    testRuntimeOnly("org.springframework.boot:spring-boot-starter-web")
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation(platform(libs.spring.boot.v2.bom.get()))
     testImplementation("org.assertj:assertj-core")
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.springframework.boot:spring-boot-starter-web")
 }
