5b6ef0c4bdfab946483776672f5dcbbca6ca9460
Alexey Lapin <alexey-lapin@protonmail.com>
2023-03-26 17:24:57 +0200

    add spotless

--- gradle/plugins/build.gradle.kts
+++ gradle/plugins/build.gradle.kts
@@ -7,5 +7,5 @@ repositories {
 }
 
 dependencies {
-    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.17.0")
+    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.13.0")
 }
